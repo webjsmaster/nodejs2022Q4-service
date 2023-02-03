9c65f05c6411247c82d22708a7e773f64f53878c
web-js-master <web-js-master@gmail.com>
2023-02-03 21:33:27 +0300

    feat: app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB } from './DB/db.service';
+import { FavoriteModule } from './favorites/favorite.module';
+import { TracksController } from './tracks/tracks.controller';
+import { TracksService } from './tracks/tracks.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, FavoriteModule],
+  controllers: [AppController, TracksController],
+  providers: [AppService, TracksService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
+  console.log('Server started on: ', port);
 }
 bootstrap();
