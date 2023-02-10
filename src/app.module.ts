1178c5b05500d892abcfec9c1c74ade7b2f996fb
web-js-master <web-js-master@gmail.com>
2023-02-10 17:18:19 +0300

    feat: Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { TracksModule } from './tracks/tracks.module'
 import { UsersModule } from './users/users.module'
 import { AlbumsModule } from './albums/albums.module'
 import { ArtistsModule } from './artists/artists.module'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigService } from '@nestjs/config/dist'
 
 @Module({
   imports: [
@@ -12,6 +15,25 @@ import { ArtistsModule } from './artists/artists.module'
     TracksModule,
     AlbumsModule,
     ArtistsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'postgres'>('TYPEORM_CONNECTION'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
